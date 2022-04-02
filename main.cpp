23ee7379d820f1caf590629d793ed2cbe1957e59
programmerkid <mohame171819@gmail.com>
2022-04-02 22:05:43 +0200

    This is basic files for application (Mohamed Wael)

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/**
+ * FCAI - programming 1 - 2022 - Assignment 3
+ * Program Name :  mian.cpp
+ * Last Modification Date : 02/4/2022
+ * Author1 and ID and Group:  Mohamed Wael Mohamed  20210361    S21
+ * Author2 and ID and Group:  Ali Tarek             20210245    S21
+ * Author3 and ID anf Group:  Omar Mohamed          20210270     S21    
+ * Teaching Assistant:
+ * Purpose: This is photoshop console app
+ */
+
+// include all libraries
+#include <bits/stdc++.h>
+
+// library  for manipulate images
+#include "bmplib.cpp"
+
+//functions declarations
+void Display_menu();
+void load();
+void save();
+
+
+int main(){
+    
+}
