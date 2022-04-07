0bd5fccb116f05e6276fa11f08a6b9080c971520
Ali-Tarek <alitarek115.at@gmail.com>
2022-04-07 15:16:37 +0200

    filter 3 (merge) & filter 6 (darken and ligthen) added

--- main.cpp
+++ main.cpp
@@ -2,76 +2,72 @@
  * FCAI - programming 1 - 2022 - Assignment 3
  * Program Name :  mian.cpp
  * Last Modification Date : 02/4/2022
- * Author1 and ID and Group:  Mohamed Wael Mohamed  20210361    S21
- * Author2 and ID and Group:  Ali Tarek             20210245    S21
- * Author3 and ID anf Group:  Omar Mohamed          20210270     S21    
+ * Author1 and ID and Group:  Mohamed Wael Mohamed      20210361    S21
+ * Author2 and ID and Group:  Ali Tarek Mohamed         20210245    S21
+ * Author3 and ID anf Group:  Omar Mohamed Gamal        20210270    S21
  * Teaching Assistant:
  * Purpose: This is photoshop console app
  */
 
 // include all libraries
 #include <bits/stdc++.h>
-
-
+#include "bmplib.cpp"
 
 using namespace std;
 
-// library  for manipulate images
-#include "bmplib.cpp"
-
-//functions declarations
+// functions declarations
 char displayMenu();
 void load();
 void save();
 void applyFilter(int choice);
 void blackWhite();
-void flip(char direction);
+void Darken_and_Lighten_Image(char);
+void merge();
 void invert();
+void flip(char direction);
 void rotate(string degree);
 void cpyToImage(unsigned char image[][SIZE]);
-
-
 
 // image that will be process
 unsigned char image[SIZE][SIZE];
 
-
-
 // start of program
-int main(){
+int main()
+{
     // load image into 2d array
     load();
 
-    while (true){
-        //pass choice of user to applyfilter
-        applyFilter( displayMenu() );
+    while (true)
+    {
+        // pass choice of user to applyfilter
+        applyFilter(displayMenu());
     }
-    
-   
 }
 
 // load image to 2d array in memory
-void load(){
+void load()
+{
 
     fstream file;
-    char fileName[100]; 
+    char fileName[100];
 
-    // loop until user enter valid image 
-    do{
+    // loop until user enter valid image
+    do
+    {
         cout << "please enter file name of the image to process: ";
         cin >> fileName;
 
-
         strcat(fileName, ".bmp");
-        //see if the image exists or not
+        // see if the image exists or not
         file.open(fileName);
-    }while(!file);
-        
-    readGSBMP(fileName, image);    
+    } while (!file);
+
+    readGSBMP(fileName, image);
 }
 
 // create menu for user
-char displayMenu(){
+char displayMenu()
+{
 
     cout << "Please select a filter to apply or 0 to exit: \n";
     cout << "1- Black & white Filter\n";
@@ -88,23 +84,30 @@ char displayMenu(){
     cout << "c- Blur Image\n";
     cout << "s- Save the image to a file\n";
     cout << "0- Exit\n";
-    
-    //filter to apply
+
+    // filter to apply
     char choice;
     cout << ">> ";
     cin >> choice;
 
     return tolower(choice);
-
 }
 
-//call filter 
-void applyFilter(int choice){
+// call filter
+void applyFilter(int choice)
+{
 
     switch (choice)
     {
     case '1':
         blackWhite();
+        break;
+
+    case '5':
+        char contrast;
+        cout << "Contrast (D)arken or (L)ighten: ";
+        cin >> contrast;
+        Darken_and_Lighten_Image(contrast);
         break;
     case '0':
         cout << "GOODBYE :)\n";
@@ -112,9 +115,13 @@ void applyFilter(int choice){
         break;
     case '4':
         char direction;
-        cout << "Flip (h)orizontally or (v)ertically: "; // take direction from user
+        cout << "Flip (H)orizontally or (V)ertically: "; // take direction from user
         cin >> direction;
         flip(direction);
+        break;
+
+    case '3':
+        merge();
         break;
     case '2':
         invert();
@@ -126,103 +133,172 @@ void applyFilter(int choice){
         cin >> degree;
         rotate(degree);
     }
-        break;
+    break;
     case 's':
         save();
         break;
     default:
         cout << "Not available Filter\n";
         break;
-    //filter to apply
-    char choice;
-    cout << ">> ";
-    cin >> choice;
+        // filter to apply
+        char choice;
+        cout << ">> ";
+        cin >> choice;
     }
-
 }
 
-
-void save(){
-    char  file[100];
-    cout << "Please enter target file name: "; 
+void save()
+{
+    char file[100];
+    cout << "Please enter target file name: ";
     cin >> file;
     cout << '\n';
     strcat(file, ".bmp");
     writeGSBMP(file, image);
 }
 
-
-
 // convert image to black and white image
-void blackWhite(){
+void blackWhite()
+{
 
     // calcualte average
-    long long  avg = 0;
-    for(int i=0; i<SIZE; i++){
-        for(int j=0; j<SIZE; j++){
+    long long avg = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
             avg += image[i][j];
         }
     }
 
     avg /= 256 * 256;
 
-    
     unsigned char blackWhiteImage[SIZE][SIZE];
 
     // convert to black and white image
-    for(int i=0; i<SIZE; i++){
-        for(int j=0; j<SIZE; j++){
-            if( image[i][j] > avg){
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (image[i][j] > avg)
+            {
                 blackWhiteImage[i][j] = 255;
-            }else{
+            }
+            else
+            {
                 blackWhiteImage[i][j] = 0;
             }
         }
     }
-    
 
     cpyToImage(blackWhiteImage);
 }
 
-void flip(char direction){
+
+void Darken_and_Lighten_Image(char contrast)
+{
+    unsigned char adjusted_image[SIZE][SIZE]; // dorl means darken or ligthen
+
+    if(contrast == 'D' || contrast == 'd') // darken the image
+    {
+        for(int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                adjusted_image[i][j] = 0.5 * image[i][j]; 
+    }
+
+
+    else if(contrast == 'L' || contrast == 'l')  // ligthen the image
+    {
+        for(int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                adjusted_image[i][j]=pow(image[i][j],0.8)+150; 
+    }
+
+
+    cpyToImage(adjusted_image);
+}
+
+void merge()
+{
+    fstream file;
+    char fileName2[100];
+    unsigned char image2[SIZE][SIZE];
+
+    do
+    {
+        cout << "please enter file name of the second image that you want to merge: ";
+        cin >> fileName2;
+
+        strcat(fileName2, ".bmp");
+        // see if the image exists or not
+        file.open(fileName2);
+    } while (!file);
+
+    readGSBMP(fileName2, image2);
+
+    unsigned char merged_image[SIZE][SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            merged_image[i][j] = (image[i][j] + image2[i][j]) / 2;
+        
+    cpyToImage(merged_image);
+}
+
+
+
+void flip(char direction)
+{
 
     unsigned char flipImage[SIZE][SIZE];
-    
-    //flip vertical
-    if( direction == 'v'){
-        for(int i=0; i<SIZE; i++){
-            for(int j=0; j<SIZE; j++){
-                flipImage[i][j] = image[255-i][j];
+
+    // flip vertical
+    if (direction == 'V' || direction == 'v')
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                flipImage[i][j] = image[255 - i][j];
             }
         }
-    }else if( direction == 'h'){ // flip horizontal
-        for(int i=0; i<SIZE; i++){
-            for(int j=0; j<SIZE; j++){
-                flipImage[i][j] = image[i][255-j];
+    }
+    else if (direction == 'H' || direction == 'h')
+    { // flip horizontal
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                flipImage[i][j] = image[i][255 - j];
             }
         }
     }
 
-    //apply filter to image
+    // apply filter to image
     cpyToImage(flipImage);
-    
 }
 
-//copy image after filter to globall image
-void cpyToImage(unsigned char filterImage[][SIZE]){
-    for(int i=0; i<SIZE; i++){
-        for(int j=0; j<SIZE; j++){
+// copy image after filter to globall image
+void cpyToImage(unsigned char filterImage[][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
             image[i][j] = filterImage[i][j];
         }
     }
 }
 
-//invert filter
-void invert(){
+// invert filter
+void invert()
+{
     unsigned char invertImage[SIZE][SIZE];
-    
-    for(int i=0; i<SIZE; i++){
-        for(int j=0; j<SIZE; j++){
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
             invertImage[i][j] = 255 - image[i][j];
         }
     }
@@ -231,30 +307,36 @@ void invert(){
 }
 
 // rotate image by specific degree
-void rotate(string degree){
+void rotate(string degree)
+{
 
     unsigned char rotatedImage[SIZE][SIZE];
 
-    // rotate by 90 
-    if( degree == "90"){
+    // rotate by 90
+    if (degree == "90")
+    {
 
         // transpose image matrix
-        for(int i=0; i<SIZE; i++){
-            for(int j=0; j<SIZE; j++){
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
                 rotatedImage[i][j] = image[j][i];
             }
-        }    
-
-    }else if( degree == "180"){ // rotate by 180
+        }
+    }
+    else if (degree == "180")
+    { // rotate by 180
 
         flip('v');
         flip('h');
         return;
-    }else if( degree == "270"){
+    }
+    else if (degree == "270")
+    {
         rotate("90");
         flip('h');
         return;
-        
     }
 
     cpyToImage(rotatedImage);
